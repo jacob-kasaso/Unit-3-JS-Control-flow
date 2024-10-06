de161466fcc8ebafdf04a12940d55f66838ccef9
jacob-kasaso <jacobkasaso@gmail.com>
2024-10-06 18:42:59 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <= 400){
+    return 'This one is on me!'
+  }if(feet > 400 && feet < 2000){
+    return 'That will be twenty bucks.'
+  }else if(feet > 2000 && feet < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }else{
+    return 'No can do.'
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === "NYC") ? "Ok, sounds good." : "No go.";
 }
+console.log(ternaryCheckCity('NYC'))
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tips){
   // Write your code here!
+  switch (tips){
+    case 'generous':
+    return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+      default: 'if anything else'
+      return 'Bye.'
+  }
+
 }
